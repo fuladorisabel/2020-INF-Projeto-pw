a331c1af3ad6dbc751d696b14448a9c2e46f3e4e
Aluno <ingridfulador234@gmail.com>
2020-03-02 09:33:50 -0300

    first commit

---
+++ aula03.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <style media="screen">
+     .noticias h2 {
+       color : 006600;
+       background-color: #DFDFDF;
+       font-size :20px;
+     }
+     .noticias img {
+       width: 300px;
+       /* largura */
+       height: 300px;
+       /* altura */
+     }
+  </style>
+</head>
+<body>
+
+  <h1>Portal de notícias IFPR</h1>
+
+  <div class="noticias">
+    <h2>Tromba d'água impressiona banhistas no litoral do Paraná</h2>
+    <img src="https://paranaportal.uol.com.br/wp-content/uploads/2020/02/tubarao-azul-guaratuba.jpg" alt="">
+    <p>Uma tromba d'água registrada em Caiobá e no balneário Inajá, em Matinhos, no litoral do Paraná, impressionou banhistas </p>
+    <p>Segundo especialistas da Somar Meteorologia, a tromba d'água é um tornado que se forma sobre algum corpo de água e está associado a correntes de ventos em rotação,
+      em nuvens de tempestades intensas e de chuva.</p>
+      <p>Os especialistas ressaltam que o fenômeno, no Brasil, é muito mais comum do que o tornado, principalmente nas regiões sul e sudeste. A grande maioria das trombas d'água,
+        conforme a Somar, são de pequenas formações.</p>
+      </div>
+
+      <div class="noticias">
+        <h2>Segundo caso de novo coronavírus no Brasil é confirmado em São Paulo</h2>
+        <img src="https://s2.glbimg.com/wzMn5T198Y7qiI9Roz9dfrOPzco=/0x0:1800x1406/1600x0/smart/filters:strip_icc
+        ()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2020/S/r/cpazPGQAytk0HH7VyXRA/coronavirus-visao-microscopio.jpg" alt="">
+        <p>Em nota, o ministério informa que o paciente sentiu os sintomas no mesmo dia do retorno ao Brasil:
+          ele relatou febre, tosse, dor de garganta, dor muscular e dor de cabeça. Segundo as autoridades brasileiras,
+          o homem vestiu máscara durante todo o voo de volta a São Paulo.</p>
+          <p>Na sexta-feira, o paciente procurou atendimento no Hospital Albert Einstein, onde fez exames. O resultado confirmou Covid-19
+            — nome da doença do novo coronavírus.</p>
+            <table style="width:40%">
+              <tr>
+                <th>UF	</th>
+                <th>Casos suspeitos</th>
+                <th>Casos descartados</th>
+              </tr>
+              <tr>
+                <td>MG</td>
+                <td>17</td>
+                <td>03</td>
+
+              </tr>
+              <tr>
+                <td>PR</td>
+                <td>05</td>
+                <td>03</td>
+              </tr>
+              <tr>
+                <td>RJ</td>
+                <td>19</td>
+                <td>08</td>
+              </tr>
+              <tr>
+                <td>SP</td>
+                <td>91</td>
+                <td>38</td>
+              </tr>
+            </table>
+          </div>
+
+          <div class="noticias">
+            <h2>Prorrogada MP de auxílio para pescador afetado por óleo</h2>
+            <img src="https://imagens.ebc.com.br/CeNEIfuAoVQ2rkfZ0kjp26ZUZCM=/1170x700/smart/http://agenciabrasil.ebc.com.br/sites/default/files/thumbnails/image/0873884aca8038405e40e8af10b7b28b.jpg?itok=_exiM75J" alt="">
+            <p>O auxílio emergencial beneficia pescadores que atuam em municípios dos nove estados do Nordeste,
+               do Rio de Janeiro e do Espírito Santo atingidos pelo vazamento de óleo.</p>
+            <p>A primeira parcela, de R$ 998, foi paga em dezembro. Esse benefício não interfere no recebimento do seguro-defeso recebido pelos profissionais na época em que é proibida a pesca para garantir a reprodução dos peixes.</p>
+          </div>
+        </body>
+        </html>
