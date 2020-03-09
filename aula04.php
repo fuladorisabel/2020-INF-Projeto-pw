40543293d9d8ec78b74d22c9f0cb80a9cf31c225
Aluno <ingridfulador234@gmail.com>
2020-03-09 09:25:21 -0300

    aula04

---
+++ aula04.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Aula 04 - CSS e HTML</title>
+    <link href="https://fonts.googleapis.com/css?family=Dancing+Script|Playfair+Display&display=swap" rel="stylesheet">
+   <link rel="stylesheet" href="aula04.css">
+  </head>
+  <body>
+ <div class="site">
+   <div class="topo">
+    <h3> Projeto CSS</h3>
+   </div>
+   <div class="conteudo">
+     corpo do site
+   </div>
+   <div class="rodape">
+     Desenvolvido por Isabel Fulador
+   </div>
+
+ </div>
+  </body>
+</html>
