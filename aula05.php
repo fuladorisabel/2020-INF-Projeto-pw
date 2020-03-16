52998d8fbcd93245ef1e7cffb12c45aa34fa8f14
Aluno <ingridfulador234@gmail.com>
2020-03-16 10:01:32 -0300

    05aula

--- aula05.php
+++ aula05.php
@@ -4,20 +4,26 @@
     <meta charset="utf-8">
     <title>Aula 04 - CSS e HTML</title>
     <link href="https://fonts.googleapis.com/css?family=Dancing+Script|Playfair+Display&display=swap" rel="stylesheet">
-   <link rel="stylesheet" href="aula04.css">
+   <link rel="stylesheet" href="aula05.css">
   </head>
   <body>
  <div class="site">
    <div class="topo">
   <h1>Portal de notícias IFPR</h1>
+  <div class="menu">
+    <ul>
+      <li><a href="#">Página inicial</a></li>
+      <li><a href="#">Cadastro</a></li>
+      <li><a href="#">Contato</a></li>
+      <li><a href="#">Ajuda</a></li>
+      <li><a href="#">Login</a></li>
+    </ul>
+  </div>
    </div>
    <div class="conteudo">
      <div class="noticias">
-       <a href="#" class="botao botao-azul">Botão Azul</a>
-       <a href="#" class="botao botao-verde">Botão verde</a>
-       <a href="#" class="botao botao-vermelho">Botão vermelho</a>
        <h2>Tromba d'água impressiona banhistas no litoral do Paraná</h2>
-       <img src="https://paranaportal.uol.com.br/wp-content/uploads/2020/02/tubarao-azul-guaratuba.jpg"  rel="stylesheet">
+       <img  width="300" src="https://paranaportal.uol.com.br/wp-content/uploads/2020/02/tubarao-azul-guaratuba.jpg"  rel="stylesheet">
        <p>Uma tromba d'água registrada em Caiobá e no balneário Inajá, em Matinhos, no litoral do Paraná, impressionou banhistas </p>
        <p>Segundo especialistas da Somar Meteorologia, a tromba d'água é um tornado que se forma sobre algum corpo de água e está associado a correntes de ventos em rotação,
          em nuvens de tempestades intensas e de chuva.</p>
@@ -27,10 +33,9 @@
 
          <div class="noticias">
            <h2>Segundo caso de novo coronavírus no Brasil é confirmado em São Paulo</h2>
-           <img src="https://s2.glbimg.com/wzMn5T198Y7qiI9Roz9dfrOPzco=/0x0:1800x1406/1600x0/smart/filters:strip_icc
+           <img width="300" src="https://s2.glbimg.com/wzMn5T198Y7qiI9Roz9dfrOPzco=/0x0:1800x1406/1600x0/smart/filters:strip_icc
            ()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2020/S/r/cpazPGQAytk0HH7VyXRA/coronavirus-visao-microscopio.jpg"  rel="stylesheet">
-           <p>Em nota, o ministério informa que o paciente sentiu os sintomas no mesmo dia do retorno ao Brasil:
-             ele relatou febre, tosse, dor de garganta, dor muscular e dor de cabeça. Segundo as autoridades brasileiras,
+           <p>Em nota, o ministério informa que o paciente sentiu os sintomas no mesmo dia do retorno ao Brasil:ele relatou febre, tosse, dor de garganta, dor muscular e dor de cabeça. Segundo as autoridades brasileiras,
              o homem vestiu máscara durante todo o voo de volta a São Paulo.</p>
              <p>Na sexta-feira, o paciente procurou atendimento no Hospital Albert Einstein, onde fez exames. O resultado confirmou Covid-19
                — nome da doença do novo coronavírus.</p>
@@ -66,11 +71,12 @@
 
              <div class="noticias">
                <h2>Prorrogada MP de auxílio para pescador afetado por óleo</h2>
-               <img src="https://imagens.ebc.com.br/CeNEIfuAoVQ2rkfZ0kjp26ZUZCM=/1170x700/smart/http://agenciabrasil.ebc.com.br/sites/default/files/thumbnails/image/0873884aca8038405e40e8af10b7b28b.jpg?itok=_exiM75J" alt="">
+               <img width="300" src="https://imagens.ebc.com.br/CeNEIfuAoVQ2rkfZ0kjp26ZUZCM=/1170x700/smart/http://agenciabrasil.ebc.com.br/sites/default/files/thumbnails/image/0873884aca8038405e40e8af10b7b28b.jpg?itok=_exiM75J" alt="">
                <p>O auxílio emergencial beneficia pescadores que atuam em municípios dos nove estados do Nordeste,
                   do Rio de Janeiro e do Espírito Santo atingidos pelo vazamento de óleo.</p>
                <p>A primeira parcela, de R$ 998, foi paga em dezembro. Esse benefício não interfere no recebimento do seguro-defeso recebido pelos profissionais na época em que é proibida a pesca para garantir a reprodução dos peixes.</p>
              </div>
+   </div>
    </div>
    <div class="rodape">
      Desenvolvido por Isabel Fulador
